bc5acb5e85b082019649fc6067c1cdbb000e518a
Luke.Humphreys <lhumphreys67@gmail.com>
2018-10-22 22:45:13 +0100

    More page load tests

--- test/page_load.cpp
+++ test/page_load.cpp
@@ -5,24 +5,79 @@
 #include <OSTools.h>
 #include <PipePublisher.h>
 #include <CefTestJSBaseTest.h>
+#include <ReqServer.h>
+#include <OpenConnections.h>
+#include <WorkerThread.h>
 
+/**
+ * The Load Handler receives callbacks on both the Render and the UI process.
+ *
+ * This allows us to provdie two publishers:
+ *    - A PipePublisher publishing to a ring buffer in the tests' local process
+ *    - A subscription server, publishing from the browser process
+ *
+ *  Crucially the root from the browser process to the test thread doesn't pass
+ *  through the  TID_RENDERER at all.
+ */
 class PageLoadHook: public CefLoadHandler
 {
 public:
+    static constexpr int port = 1250;
+    static constexpr const char* url = "ws://localhost:1250";
+
+    PageLoadHook()
+    {
+        if (CefCurrentlyOn(TID_UI)) {
+            server.AddHandler(LoadedServer::SUB_NAME, std::make_shared<LoadedServer>());
+            serverThread.PostTask([&] () -> void {
+                server.HandleRequests(port);
+            });
+            serverThread.Start();
+            server.WaitUntilRunning();
+        }
+    }
+
+    /**
+     * CEF Hook that will be triggered on both the browser and the render process
+     */
     void OnLoadEnd(CefRefPtr<CefBrowser> browser,
-                           CefRefPtr<CefFrame> frame,
-                           int httpStatusCode) override
-   {
+                   CefRefPtr<CefFrame> frame,
+                   int httpStatusCode) override
+    {
         if (CefCurrentlyOn(TID_RENDERER)) {
             urlPub.Publish(frame->GetURL());
+        } else if (CefCurrentlyOn(TID_UI)) {
+            urlSubPub->Publish(frame->GetURL());
         }
-   }
+    }
 
+    /**
+     * Subscription server publishing the urls from the browser process
+     */
+    class LoadedServer: public SubscriptionHandler {
+    public:
+        static constexpr const char* SUB_NAME = "SUB_LOADS";
+        void OnRequest(RequestHandle hdl) override {
+            clients.Add(hdl);
+        }
+        void Publish (const std::string& url) {
+            clients.Publish(url);
+        }
+    private:
+        OpenConnectionsList clients;
+    };
+
+    /**
+     * Creeat a new client ring buffer for receiving urls on the client thread
+     */
    std::shared_ptr<PipeSubscriber<std::string>> NewClient() {
         return urlPub.NewClient(1000);
     }
 private:
     PipePublisher<std::string> urlPub;
+    WorkerThread serverThread;
+    RequestServer server;
+    std::shared_ptr<LoadedServer> urlSubPub;
     IMPLEMENT_REFCOUNTING(PageLoadHook);
 };
 
@@ -30,7 +85,6 @@ private:
 CefBaseApp* app;
 std::shared_ptr<PipeSubscriber<std::string>> urls;
 std::string rootPath;
-
 
 int main (int argc, char** argv) {
     rootPath = OS::Dirname(OS::GetExe());
@@ -87,6 +141,10 @@ public:
         });
     }
 
+};
+
+class RendererNavTest: public NavigateTest {
+public:
     // Wait until we've seen another url
     static std::string WaitForPageLoad() {
         std::string url;
@@ -107,7 +165,6 @@ public:
         }
     }
 
-
     void SetUp() override {
         auto url = BuildURL("index.html");
         RequestNavigate(url);
@@ -123,13 +180,14 @@ public:
         // Check that we exhausted the published pages...
         ASSERT_FALSE(urls->GetNextMessage(url)) << url;
     }
+
 };
 
-TEST_F(NavigateTest, NoChange) {
+TEST_F(RendererNavTest, NoChange) {
     // Nothing to do...
 }
 
-TEST_F(NavigateTest, PageLoad) {
+TEST_F(RendererNavTest, PageLoad) {
     auto url = BuildURL("dummy.html");
     RequestNavigate(url);
 
@@ -138,7 +196,7 @@ TEST_F(NavigateTest, PageLoad) {
     ASSERT_EQ(current, url);
 }
 
-TEST_F(NavigateTest, HtmlRedirect) {
+TEST_F(RendererNavTest, HtmlRedirect) {
     std::string url = BuildURL("redirect.html");
     std::string final = BuildURL("index.html");
     RequestNavigate(url);
